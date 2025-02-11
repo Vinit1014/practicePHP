fa8d2bb33f2bf8b0f730b936d3002e307be40632
Vinit <vinits1014@gmail.com>
2025-02-11 18:14:17 +0530

    Trying it

--- index.php
+++ index.php
@@ -4,4 +4,15 @@
     echo "Hello world";
     var_dump($var2);
     echo $var2;
+    $str = "<h1>Hello World!</h1>";
+    $newstr = filter_var($str, FILTER_SANITIZE_STRING);
+    echo $newstr;
+
+    $int = '0i';
+    if (filter_var($int, FILTER_VALIDATE_INT) === 0 || !filter_var($int, FILTER_VALIDATE_INT) === false) {
+    echo("Integer is valid");
+    } else {
+    echo("Integer is not valid");
+    }
+
 ?>
